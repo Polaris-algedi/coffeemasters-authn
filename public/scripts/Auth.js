91c7b77fe3b3bb2355101a36b0d065b77f87f005
Polaris Algedi <karchalmohammed@gmail.com>
2024-08-01 20:28:37 +0100

    chore: Refactor Auth.js for improved registration and login functionality

--- public/scripts/Auth.js
+++ public/scripts/Auth.js
@@ -2,37 +2,57 @@ import API from "./API.js";
 import Router from "./Router.js";
 
 const Auth = {
-    isLoggedIn: false,
-    account: null,
-    updateStatus() {
-        if (Auth.isLoggedIn && Auth.account) {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "none"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".account_name").forEach(
-                e => e.innerHTML = Auth.account.name
-            );
-            document.querySelectorAll(".account_username").forEach(
-                e => e.innerHTML = Auth.account.email
-            );
+  isLoggedIn: false,
+  account: null,
+  register: async (event) => {
+    event.preventDefault();
+    const user = {
+      name: event.target["register_name"].value, // document.getElementById("register_name").value,
+      email: event.target["register_email"].value, // document.getElementById("register_email").value,
+      password: event.target["register_password"].value, // document.getElementById("register_password").value,
+    };
 
-        } else {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "none"
-            );
+    const result = await API.register(user);
+    console.log(result);
+  },
 
-        }
-    },    
-    init: () => {
-        
-    },
-}
+  login: async (event) => {
+    event.preventDefault();
+
+    const user = {
+      email: event.target["login_email"].value, // document.getElementById("login_email").value,
+      password: event.target["login_password"].value, // document.getElementById("login_password").value,
+    };
+
+    const result = await API.login(user);
+    console.log(result);
+  },
+
+  updateStatus() {
+    if (Auth.isLoggedIn && Auth.account) {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "none"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".account_name")
+        .forEach((e) => (e.innerHTML = Auth.account.name));
+      document
+        .querySelectorAll(".account_username")
+        .forEach((e) => (e.innerHTML = Auth.account.email));
+    } else {
+      document
+        .querySelectorAll(".logged_out")
+        .forEach((e) => (e.style.display = "block"));
+      document
+        .querySelectorAll(".logged_in")
+        .forEach((e) => (e.style.display = "none"));
+    }
+  },
+  init: () => {},
+};
 Auth.updateStatus();
 
 export default Auth;
